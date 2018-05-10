134e5810dc6a289dad22145aab6f91fe12b6b104
Xuanyu Hu <helium729@126.com>
2018-05-10 10:25:58 +0800

    initialize args

--- sources/main.c
+++ sources/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define WINDOWS 1
 #define LINUX 0
@@ -8,6 +9,14 @@
 //Main entrance of the program
 int main(int argc, char * argv[])
 {
+    if(argc != 3)
+    {
+        printf("Usage:\nfidia [inputPath] [outputPath]\n");
+        //Program returns with args error
+        return -1;
+    }
+    char *inputPath = argv[1];
+    char *outputPath = argv[2];
     //ToDo
     return 0;
 }
