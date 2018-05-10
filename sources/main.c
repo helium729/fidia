1fb561fdf962539cbfd269c20f0ae4b6116b699c
Xuanyu Hu <helium729@126.com>
2018-05-10 10:15:08 +0800

    first commit

---
+++ sources/main.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WINDOWS 1
+#define LINUX 0
+
+//Main entrance of the program
+int main(int argc, char * argv[])
+{
+    //ToDo
+    return 0;
+}
