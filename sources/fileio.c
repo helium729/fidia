a13e894368d6ed502a2907ad4436d103321b6cd7
Xuanyu Hu <helium729@126.com>
2018-05-19 17:20:44 +0800

    add read single byte func

--- sources/fileio.c
+++ sources/fileio.c
@@ -1,1 +1,24 @@
 #include "fileio.h"
+
+unsigned char readSingleByte (char* path, long int location)
+{
+
+	FILE *fp = NULL;
+	unsigned char ch[1];
+	fp = fopen(path,"rb");
+	
+	/*如果无法读取*/ 
+	if (fp==NULL)      
+	{
+	    printf("File open error!\n");
+	    exit(1);
+	}
+	
+	/*如果输入的位置（long int）第一个字符为字母或汉字则返回位置0的1字节*/ 
+	
+	fseek(fp, location, 0);  //找到文件中的目标位置 
+	fread(ch, sizeof(ch), 1, fp); //读取一个字节
+	
+	fclose(fp);
+	return ch[0];
+}
